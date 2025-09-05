79f23d76214961cbd8c2f13df3354dc63eded227
Giovanni Guerrieri <49747129+Soap2G@users.noreply.github.com>
2025-09-05 16:38:36 +0200

    feat: add FCC configuration

--- etc/jupyter/jupyter_server_config.py
+++ etc/jupyter/jupyter_server_config.py
@@ -2,16 +2,9 @@ import json
 
 # CERN-VRE - Configure the JupyterLab server - TRIGGERED TO ALWAYS WORK
 use_rucio_extension = os.environ.get('USE_RUCIO_EXTENSION', 'true').lower() == 'true'
-
 if use_rucio_extension:
 
     def write_jupyterlab_config():
-        """"
-        Creates a JSON file with the different Rucio instance configurations,
-        needed by the Rucio JupyterLab extension.
-        The file must live in '~/.jupyter/jupyter_server_config.json'.
-        
-        """
         dir_config_jupyterlab = os.getenv('JUPYTER_CONFIG_DIR',
                                         os.path.join(os.getenv('HOME'), '.jupyter')
                                         )
@@ -71,6 +64,26 @@ if use_rucio_extension:
             "mode": os.getenv('ATLAS_RUCIO_MODE', 'replica'),
             "wildcard_enabled": os.getenv('ATLAS_RUCIO_WILDCARD_ENABLED', '1') == '1',
         }
+        fcc_config = {
+            "name": os.getenv('FCC_RUCIO_NAME', 'fcc.rucioit.cern.ch'),
+            "display_name": os.getenv('FCC_RUCIO_DISPLAY_NAME', 'FCC RUCIO'),
+            "rucio_base_url": os.getenv('FCC_RUCIO_BASE_URL', 'https://fcc-server.rucioit.cern.ch'),
+            "rucio_auth_url": os.getenv('FCC_RUCIO_AUTH_URL', 'https://fcc-auth.rucioit.cern.ch'),
+            "rucio_webui_url": os.getenv('FCC_RUCIO_WEBUI_URL', 'https://fcc-webui.rucioit.cern.ch'),
+            "rucio_ca_cert": os.getenv('RUCIO_CA_CERT', '/eos/user/e/engarcia/rucio_ca_certs/rucio_ca.pem'),
+            "site_name": os.getenv('FCC_RUCIO_SITE_NAME', 'CERN'),
+            "vo": os.getenv('FCC_RUCIO_VO', 'fcc'),
+            "voms_enabled": os.getenv('RUCIO_VOMS_ENABLED', '0') == '1',
+            "voms_vomses_path": os.getenv('RUCIO_VOMS_VOMSES_PATH', '/etc/vomses'),
+            "voms_certdir_path": os.getenv('RUCIO_VOMS_CERTDIR_PATH',),
+            "voms_vomsdir_path": os.getenv('RUCIO_VOMS_VOMSDIR_PATH', '/etc/grid-security/vomsdir'),
+            "destination_rse": os.getenv('FCC_RUCIO_DESTINATION_RSE', 'FCC_PROD_PHYS_TOP'),
+            "rse_mount_path": os.getenv('FCC_RUCIO_RSE_MOUNT_PATH', '/eos/workspace/r/rucioit/FCC_PROD_PHYS_TOP'),
+            "replication_rule_lifetime_days": int(os.getenv('FCC_RUCIO_REPLICATION_RULE_LIFETIME_DAYS')) if os.getenv('FCC_RUCIO_REPLICATION_RULE_LIFETIME_DAYS') else None,
+            "path_begins_at": int(os.getenv('FCC_RUCIO_PATH_BEGINS_AT', '5')),
+            "mode": os.getenv('FCC_RUCIO_MODE', 'replica'),
+            "wildcard_enabled": os.getenv('FCC_RUCIO_WILDCARD_ENABLED', '1') == '1',
+        }
 
 
         escape_config = {k: v for k,
@@ -79,8 +92,11 @@ if use_rucio_extension:
         atlas_config = {k: v for k,
                         v in atlas_config.items() if v is not None}
 
+        fcc_config = {k: v for k,
+                        v in fcc_config.items() if v is not None}
+
         config_json['RucioConfig'] = {
-            'instances': [escape_config, atlas_config], # cms,
+            'instances': [escape_config, atlas_config, fcc_config], # cms,
             "default_instance": os.getenv('RUCIO_DEFAULT_INSTANCE', escape_config['name']),
             "default_auth_type": os.getenv('RUCIO_DEFAULT_AUTH_TYPE', 'x509_proxy'),
             "log_level": os.getenv('RUCIO_LOG_LEVEL', 'debug'),
@@ -91,14 +107,6 @@ if use_rucio_extension:
         config_file.close()
 
     def write_ipython_config():
-        """"
-        Creates an ~/.ipython/profile_default/ipython_kernel_config.json file.
-
-        This configuration to links the Rucio JupyterLab extension
-        with the Jupyter Notebook kernel, allowing the propagation of data, via
-        environmental variables, from the extension to a Jupyter Notebook.
-        """
-
         file_path = os.path.join(os.getenv('HOME'), '.ipython/profile_default')
         file_name = os.path.join(file_path, 'ipython_kernel_config.json')
         extension_module = 'rucio_jupyterlab.kernels.ipython'
